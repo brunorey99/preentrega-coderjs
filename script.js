638771fab855cbe87bb7397fa9965e9f70ab29c9
Bruno Reynoso <brunoreynoso1999@gmail.com>
2023-09-15 23:22:24 -0300

    agregando funciones

--- script.js
+++ script.js
@@ -1,22 +1,93 @@
 //app de cine
 
 class Movie {
-    constructor(title, price, showtime1, showtime2, room) {
+    constructor(title, price, director, showtime1, showtime2, room, age) {
         this.title = title,
         this.price = price,
+        this.director = director,
         this.showtime1 = showtime1,
         this.showtime2 = showtime2,
-        this.room = room
+        this.room = room,
+        this.age = age
+    }
+    showInfo() {
+        console.log(`${this.title} de ${this.director} para mayores de ${this.age}.`)
     }
 }
 
-const movie1 = new Movie(title = 'Alien', price = 2200, showtime1 = '19:30', showtime2 = '21:00', room = 1);
-const movie2 = new Movie(title = 'The Shining', price = 3000, showtime1 = '19:00', showtime2 = '21:30', room = 2);
+const movie1 = new Movie('Alien', 2200, 'Ridley Scott', '19:30', '23:00', 1, 18);
+const movie2 = new Movie('The Shining', 3000, 'Stanley Kubrick', '19:00', '23:30', 2, 18);
+const movie3 = new Movie('Hereditary', 3000, 'Ari Aster', '21:30', '01:30', 1, 18);
+const movie4 = new Movie('Midsommar', 3000, 'Ari Aster', '21:00', '01:00', 2, 18);
+const movie5 = new Movie('Suspiria', 3000, 'Dario Argento', '21:30', '23:30', 3, 16);
+const movie6 = new Movie('The Thing', 3000, 'John Carpenter', '20:30', '22:30', 3, 14);
+
+const movies = [];
+movies.push(movie1, movie2, movie3, movie4, movie5, movie6);
+
+function showMovies(array) {
+    console.log('Nuestras peliculas en cartelera son:')
+    for (let movie of array) {
+        movie.showInfo()
+    }
+}
+
+// filtro de peliculas por director 
+
+function searchDir(array) {
+    let searchedDir = prompt('Ingresar director a buscar:')
+    let search = array.filter(
+        (movie) => movie.director.toLowerCase() == searchedDir.toLowerCase()
+    )
+    if (search == 0) {
+        console.log('No se encuentran resultados.')
+    } else {
+        showMovies(search)
+    }
+}
+
+// filtro de peliculas por edad
+
+function searchAge(array) {
+    let searchedAge = parseInt(prompt('Ingresar edad:'))
+    let search = array.filter(
+        (movie) => movie.age <= searchedAge
+    )
+    if (search == 0) {
+        console.log('No se encuentran resultados.')
+    } else {
+        showMovies(search)
+    }
+}
+
+// searchAge(movies)
+
+// searchDir(movies)
+
+// showMovies(movies);
 
 function movieMenu() {
-    let menu = parseInt(prompt(`Bienvenido a Cine Random!\n\n1. Funcion de ${movie1.title}.\n2. Funcion de ${movie2.title}.\n3. Quiero ver las dos.`))
+    let menu = parseInt(prompt
+        (`Bienvenido a Cine Random!
+        1. Mostrar cartelera.
+        2. Buscar pelicula por director.
+        3. Buscar pelicula por edad.
+        4. Funcion de ${movie1.title}.
+        5. Funcion de ${movie2.title}.
+        6. Quiero ver las dos.`))
 
-    if (menu == 1) {
+    
+    
+    if (menu == 1){
+        showMovies(movies)
+
+    } else if (menu == 2) {
+        searchDir(movies)
+
+    } else if (menu == 3) {
+        searchAge(movies)
+
+    } else if (menu == 4) {
         let option = prompt(`Funcion de ${movie1.title}. Tiene los siguientes horarios:\n1. ${movie1.showtime1}\n2. ${movie1.showtime2}.`)
         if (option == 1) {
             alert(`La funcion de ${movie1.title} de las ${movie1.showtime1} tiene precio de $${movie1.price} y la podes encontrar en la sala ${movie1.room}.`)
@@ -26,7 +97,7 @@ function movieMenu() {
             alert('Hubo un error, intenta de nuevo.')
         }
     
-    } else if (menu == 2) {
+    } else if (menu == 5) {
         let option2 = prompt(`Funcion de ${movie2.title}. Tiene los siguientes horarios:\n1. ${movie2.showtime1}\n2. ${movie2.showtime2}.`)
         if (option2 == 1) {
             alert(`La funcion de ${movie2.title} de las ${movie2.showtime1} tiene precio de $${movie2.price} y la podes encontrar en la sala ${movie2.room}.`)
@@ -36,7 +107,7 @@ function movieMenu() {
             alert('Hubo un error, intenta de nuevo.')
         }
     
-    } else if (menu == 3) {
+    } else if (menu == 6) {
         let option3 = prompt(`Para ver las dos tenes las siguientes opciones:\n\n1. ${movie1.title} a las ${movie1.showtime1} y ${movie2.title} a las ${movie2.showtime2}\n2. ${movie2.title} a las ${movie2.showtime1} y ${movie1.title} a las ${movie1.showtime2}`)
         if (option3 == 1) {
             alert('El precio seria de ' + (movie1.price + movie2.price) + '.' + ` En la sala ${movie1.room} a las ${movie1.showtime1} horas.`)
@@ -48,8 +119,6 @@ function movieMenu() {
     }
 }
 
-
-// estaba por entregar y me di cuenta que no habia usado ningun ciclo, no sabia como implemetarlo y me ayude con chatgpt
 
 while (true) {
     movieMenu();
